0aa61034902ff0eac8cdda0309c2ed3f593d8fca
unknown <hamzasdiri@insat.u-carthage.tn>
2022-02-23 00:21:43 +0100

    hamza-first-commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from './Footer.module.css';
+class Footer extends React.Component {
+
+    render(){
+      
+
+    return(
+      
+        <>
+        <footer>
+        <div className={styles.waves}>
+          <div className={styles.wave} id={styles.wave1} />
+          <div className={styles.wave} id={styles.wave2} />
+          <div className={styles.wave} id={styles.wave3}/>
+          <div className={styles.wave} id={styles.wave4} />
+        </div>
+        <ul className={styles.social_icon}>
+          <li><a href="#"><ion-icon name="logo-facebook" /></a></li>
+          <li><a href="#"><ion-icon name="logo-instagram" /></a></li>
+          <li><a href="#"><ion-icon name="logo-linkedin" /></a></li>
+        </ul>
+        <h2><p id={styles.contact}>CONTACT</p></h2>
+        <ul className={styles.menu}>
+          <li>Téléphone<h4 id={styles.cont}>+216 00 000 000</h4></li>
+          <li>Adresse<h4 id={styles.cont}>INSAT,centre urbain Nord, carthage, Tunisie</h4></li>
+          <li>Email<h4 id={styles.cont}>contact@robolympix.com</h4></li>
+        </ul>
+        <br /><br /><br /><br />
+        <p id={styles.fast}>© Fast &amp; Furious 4.0 - 2022</p>
+      </footer>
+        </>
+    );
+
+    }
+}
+export default Footer;
---
+++ src/Components/Sponsors.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './Sponsors.module.css'
+class Sponsors extends React.Component {
+
+    render(){
+
+    return(
+      
+        <div className="container">
+             <div style={{marginTop:-300}}></div>
+        <div className={styles.ugrey5}>
+        <h1>SPONSORS</h1>
+        <div>
+        
+        <article className={styles.row} id={styles.ideaOne}>
+          <div><img src="https://images02.nicepage.com/a1389d7bc73adea1e1c1fb7e/303f208dbbed58b0ad91b352/3.png" alt='1' /></div>
+          <div>
+            <h3>Idea 1</h3>
+            <p>Here's your descriptive text for your idea to sell people on the thing after the image gets their attention. This type of section looks best when there's enough text to take up most of the height of the adjacent image.</p>
+        
+          </div>
+        </article>
+        <article className={styles.row} id={styles.ideaTwo}>
+        <div><img src="https://images02.nicepage.com/a1389d7bc73adea1e1c1fb7e/b26eaa96084b5d63a1b8d98d/5.png" alt='1'/></div>
+          <div>
+            <h3>Idea 2</h3>
+            <p>Alternating images and text works with the natural flow of your user's eyes as they scan the page, allowing their eyes to catch on each of the images in turn. If the user's eyes catch on an image, they'll likely read the accompanying text.</p>
+        
+          </div>
+          
+        </article>
+      </div>
+      </div>
+      </div>
+      
+    );
+    }
+}
+
+export default Sponsors;
